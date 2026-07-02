cf605148e1a9e4928704c477ec2a4c422d190135
Aleksey <devmailbox111@gmail.com>
2026-07-02 21:38:23 +0500

    added static files for swagger

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from __future__ import annotations
 
 from fastapi import Response, FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import PlainTextResponse
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import PlainTextResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from routes.auth_middleware import *
 
 from routes.auth import auth_router
 from routes.files import files_router
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 
+app.mount("/public", StaticFiles(directory="public/admin"), name="static")
 
 @app.exception_handler(UnauthorizedException)
 async def unauthorized_plain_handler(
@@ -19,6 +22,15 @@ async def unauthorized_plain_handler(
 ) -> PlainTextResponse:
     return PlainTextResponse(content=str(exc.detail), status_code=exc.status_code)
 
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html() -> HTMLResponse:
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url or "/openapi.json",
+        title=app.title + " - Local Swagger UI",
+        swagger_js_url="/public/swagger-ui-bundle.js",
+        swagger_css_url="/public/swagger-ui.css",
+        swagger_favicon_url="/public/favicon.png",
+    )  # type: ignore[no-any-return]
 
 app.add_middleware(
     CORSMiddleware,
