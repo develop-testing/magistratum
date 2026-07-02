ac67dbaa014d70b3505549ada03bfa9643f5d451
Aleksey <devmailbox111@gmail.com>
2026-07-02 19:00:38 +0500

    added not auth exception

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from __future__ import annotations
 
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import PlainTextResponse
 from scalar_fastapi import get_scalar_api_reference
 
+from routes.auth_middleware import *
+
 from routes.auth import auth_router
+from routes.files import files_router
+
 
 app = FastAPI(docs_url=None, redoc_url=None)
+
+
+@app.get("/docs", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar for lorica",
+        servers=[{"url": "http://127.0.0.1:8800"}],
+    )
+
+@app.exception_handler(UnauthorizedException)
+async def unauthorized_plain_handler(request, exc: UnauthorizedException):
+    return PlainTextResponse(
+        content=str(exc.detail),
+        status_code=exc.status_code
+    )
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,12 +38,5 @@ app.add_middleware(
     allow_headers=["*"],             
 )
 
-@app.get("/docs", include_in_schema=False)
-async def scalar_html():
-    return get_scalar_api_reference(
-        openapi_url=app.openapi_url, # FastAPI сам отдаст схему в Scalar
-        title="Мой Проект API — Scalar",
-        servers=[{"url": "http://127.0.0.1:8800"}],
-    )
-
 app.include_router(auth_router)
+app.include_router(files_router, dependencies=[Depends(auth_middleware)])
---
+++ routes/auth_middleware.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from fastapi import Request, HTTPException
+from result import Ok
+
+from auth.redis_sessions import *
+from .response import *
+
+
+class UnauthorizedException(HTTPException):
+    def __init__(self, message: str = "Unauthorized"):
+        super().__init__(status_code=401, detail=message)
+
+
+async def auth_middleware(request: Request):
+    token = request.cookies.get("access_token", "")
+    
+    print(token, fetch_session_by_id(token))
+
+    match fetch_session_by_id(token):
+        case Ok():
+            return True
+        case _:
+            raise UnauthorizedException("you are not authorized")
