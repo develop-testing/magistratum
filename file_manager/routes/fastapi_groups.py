6ea796f05f6e3a3aa62d103b8bd8c1273730fdde
Aleksey <devmailbox111@gmail.com>
2026-07-06 16:33:57 +0500

    added permitions checks for create group

--- file_manager/routes/fastapi_groups.py
+++ file_manager/routes/fastapi_groups.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+
+from router.response import *
 
 from ..groups import Group, mk_group
 from ..sources.sqlalchemy_group import save_group
@@ -16,7 +18,10 @@ class CreateGroupRequest:
 
 
 @groups_router.post("/group", tags=["Groups"])
-async def create_group(body: CreateGroupRequest) -> Group:
+async def create_group(req: Request, body: CreateGroupRequest) -> Group:
+    if req.state.session.owner != "root":
+        raise Forbidden("only root can create groups")
+
     g = mk_group(body.name, body.owner, body.members).unwrap()
     save_group(g)
     return g
