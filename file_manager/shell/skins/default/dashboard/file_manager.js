e9197727c761e8a3a202b74efda91cade3501c8b
Aleksey <devmailbox111@gmail.com>
2026-07-20 17:53:36 +0500

    added edit button to dashboard list item

--- file_manager/shell/skins/default/dashboard/file_manager.js
+++ file_manager/shell/skins/default/dashboard/file_manager.js
@@ -1,54 +1,60 @@
 export class ManagerItem {
   constructor(id, type, img, name, owner, group) {
-    this.id = id;
-    this.type = type;
-    this.img = img;
-    this.name = name;
-    this.owner = owner;
-    this.group = group;
+    this.id = id
+    this.type = type
+    this.img = img
+    this.name = name
+    this.owner = owner
+    this.group = group
   }
 }
 
 export class FileManager {
   constructor(items) {
-    this.items = items;
+    this.items = items
   }
 }
 
 export const mk_manager_item = (id, type, img, name, owner, group) => {
-  return new ManagerItem(id, type, img, name, owner, group);
-};
+  return new ManagerItem(id, type, img, name, owner, group)
+}
 
-export const mk_file_manager = (items) => {
-  return new FileManager(items);
-};
+export const mk_file_manager = items => {
+  return new FileManager(items)
+}
 
 export const append_item = (m_items, item) => {
-  return mk_file_manager([...m_items.items, item]);
-};
+  return mk_file_manager([...m_items.items, item])
+}
 
-export const make_item_html = (item) => {
+export const make_item_html = item => {
   const types = {
     dir: "Директория",
     text_file: "Файл",
     broken: "Ошибка доступа",
-  };
+  }
 
   const type_classes = {
     dir: "type-dir",
     text_file: "type-file",
     broken: "type-broken",
-  };
+  }
 
   const type_links = {
     dir: "/dashboard/directory/",
     text_file: "/text_file/",
     broken: "type-broken",
-  };
+  }
 
-  const current_class = type_classes[item.type] || "type-unknown";
-  const current_name = types[item.type] || "Неизвестно";
-  const link = type_links[item.type] + encodeURIComponent(item.id);
+  const edit_links = {
+    dir: "/text_file/edit/",
+    text_file: "/text_file/edit/",
+  }
+
+  const current_class = type_classes[item.type] || "type-unknown"
+  const current_name = types[item.type] || "Неизвестно"
+  const link = type_links[item.type] + item.id
+  const edit_link = edit_links[item.type] + item.id
 
   return `<div data-type="${item.type}" data-id="${item.id}" class="file-item">
 		<div class="file-item-img">
@@ -56,10 +62,13 @@ export const make_item_html = (item) => {
 		</div>
 		<div class="file-item-content">
         <div data-node-remove class="file-item-close">&#128473;</div>
-				<div class="file-item-type ${current_class}">${current_name}</div>    
+				<div class="file-item-type ${current_class}">
+          <span>${current_name}</span> |
+          <a href="${edit_link}">Редактировать</>
+        </div>    
 				<a href="${link}" class="file-item-title">${item.name}</a>
 				<div class="file-item-owner">Владелец: ${item.owner}</div>
 				<div class="file-item-group">Группа: ${item.group}</div>
 		</div>
-	</div>`;
-};
+	</div>`
+}
